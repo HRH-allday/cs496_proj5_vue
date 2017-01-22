3cb2d150672b194808992b4c9a452ca4b945acfc
khw <khw0726@kaist.ac.kr>
2017-01-22 17:10:12 +0900

    images

--- src/components/ScrapBook_Scrap.vue
+++ src/components/ScrapBook_Scrap.vue
@@ -4,30 +4,45 @@
       v-on:selectstart.prevent
       v-on:mousedown="onMouseDown"
       v-on:mousemove="onMouseMove"
-      v-on:mouseup="onMouseUp">
+      v-on:mouseup="onMouseUp"
+      v-on:dragover.prevent
+      v-on:drop.prevent="onDrop">
       This text is displayed if your browser does not support HTML5 Canvas.
     </canvas>
-    <button id="temp"
-    v-on:click="DrawSquare"></button>
+    <!--<button id="temp"
+    v-on:click="DrawSquare"></button>-->
   </div>
 </template>
 
 <script>
   /* eslint-disable */
-  var Shape = function(x, y, w, h, fill) {
+  var Shape = function(x, y, imgSrc) {
       // This is a very simple and unsafe constructor.
       // All we're doing is checking if the values exist.
       // "x || 0" just means "if there is a value for x, use that. Otherwise use 0."
       this.x = x || 0;
       this.y = y || 0;
-      this.w = w || 1;
-      this.h = h || 1;
-      this.fill = fill || '#AAAAAA';
+      this.w = 1;
+      this.h = 1;
+      // this.imgSrc = imgSrc
+      this.img = new Image()
+      this.img.addEventListener('load', () => {
+        this.w = this.img.width
+        this.h = this.img.height
+      })
+      this.img.src = imgSrc
     }
 
     Shape.prototype.draw = function(ctx) {
-      ctx.fillStyle = this.fill;
-      ctx.fillRect(this.x, this.y, this.w, this.h);
+      // let img = new Image()
+      // img.addEventListener('load', () => {
+      // console.log(this.x+"d"+ this.y)
+      //   ctx.drawImage(img, this.x, this.y);
+      //   this.w = img.width
+      //   this.h = img.height
+      // })
+      // img.src = this.imgSrc
+      ctx.drawImage(this.img, this.x, this.y)
     }
 
     Shape.prototype.contains = function(mx, my){
@@ -48,6 +63,20 @@ export default {
         dragoffx : 0, // See mousedown and mousemove events for explanation
         dragoffy : 0
     }
+  },
+  mounted: function () {
+    // var s = document.getElementById('canvas');
+    // var ctx = s.getContext('2d');
+    this.$http.get('http://52.79.155.110:3000/getPosition').then((response) => {
+      let list = response.data
+      for(let i = 0; i< list.length; i++) {
+        let newImg = new Shape(list[i].posX, list[i].posY, decodeURIComponent(list[i].imgURL))
+        this.shapes.push(newImg)
+      }
+      this.valid = false
+    }, (response) => {
+      console.log(response)
+    })
   },
   methods:{
 
@@ -104,6 +133,22 @@ export default {
     onMouseUp: function(e){
       console.log("mouseup");
       this.dragging = false;
+    },
+
+    onDrop: function (e) {
+      var s = document.getElementById('canvas');
+      var rect = s.getBoundingClientRect();
+      e.dataTransfer.items[0].getAsString((s) => {
+        let filename = encodeURIComponent(s)
+        let posX = e.pageX - rect.left
+        let posY = e.pageY - rect.top
+        let newImg = new Shape(posX, posY, s)
+        this.shapes.push(newImg)
+        this.$http.post('http://52.79.155.110:3000/savePosition/' + filename + '/' + posX + '/' + posY).then((response) => {
+          console.log('success...?')
+        })
+        this.valid = false
+      })
     }
 
       
--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueResource from 'vue-resource'
 import App from './App'
 import '../node_modules/semantic-ui/dist/semantic.min.css'
 /* eslint-disable */
 import semantic from 'semantic'
+
+Vue.use(VueResource)
 
 /* eslint-disable no-new */
 new Vue({
