5c4ade1637eae2472b59a34abc47a98703221a82
khw <khw0726@kaist.ac.kr>
2017-01-24 22:31:45 +0900

    search

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,21 +1,48 @@
 <template>
-<div id="search">
-  <h1> 여러분의 스킬을 저장하세요 </h1>
-  <div class="ui left icon action input">
-    <i class="search icon"></i>
-    <input type="text" placeholder="Search Your Skill" v-bind:value="value" v-on:input="$emit('input', $event.target.value)" v-on:keyup.enter="$emit('mySearch')">
-    <div class="ui brown submit button" @click="$emit('mySearch')">Search</div>
-  </div>
-</div>
+  <div id="search">
+    <div class="ui container">
+      <h1> 여러분의 스킬을 저장하세요 </h1>
+      <div class="ui fluid action input">
+        <input type="text" placeholder="Search Your Skill" v-model="searchString" v-on:keyup.enter="onMySearch">
+        <div class="ui brown button" @click="onMySearch">Search</div>
+      </div>
+      <div v-if="searchSubmitted" class="ui raised segment" id="searchResults">
+        <div class="ui inverted relaxed divided list">
+          <div v-for="skill in searchResults" class="item">
+            <div class="content">
+              <a class="ui header" v-on:click="onItemClick(skill)">{{skill.name}}</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 </template>
 
-<script scoped>
+<script>
+/* eslint-disable */
 export default {
   name: 'search',
   props: ['value'],
   data () {
     return {
+      searchString: '',
+      searchSubmitted: false,
+      searchResults: []
     }
+  },
+  methods: {
+    onMySearch: function () {
+      this.$http.get('http://52.79.155.110:3000/searchSkill/' + this.searchString).then((response) => {
+        this.searchSubmitted = true
+        this.searchResults = response.body
+      })
+    },
+    onItemClick: function(skill) {
+      this.$emit('goSkill', skill)
+    }
+  },
+  mounted: function () {
+    $('#searchbox').dropdown()
   }
 }
 </script>
@@ -25,7 +52,7 @@ h1 {
   color : #ffffff ;
   font-size : 90px;
   margin-bottom : 50px;
-  alpha: 0.5;
+
 }
 
 #search{
@@ -34,6 +61,13 @@ h1 {
 
 .ui.input {
   color: #42b983;
-  width: 1000px;
+  opacity: 0.8
 }
+
+#searchResults {
+  opacity: 0.8;
+  text-align: left;
+  padding-left:3%
+}
+
 </style>
